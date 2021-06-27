bffc854a8dfa4cc7a3acbaa05fb4e538a2c4b139
Fahim Faisal <fahimfaisal.soikot@gmail.com>
2021-06-27 22:06:01 +0600

    inital commit

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import './index.css';
 
-ReactDOM.render(
+const root = document.getElementById('root');
+const app = (
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+  </React.StrictMode>
+)
+
+ReactDOM.render(app, root);
 
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
 // Learn more: https://www.snowpack.dev/concepts/hot-module-replacement
